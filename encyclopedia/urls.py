a03e4cc4596627814bd7abb5206f1bd1edb316b1
veeeetus <dawbor589@gmail.com>
2023-07-10 15:01:16 +0200

    Fix urls and add links on index

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="wiki",)
